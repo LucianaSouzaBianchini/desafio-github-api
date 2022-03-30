2d31fe7759a2b434399f0ce3e86841d2a8912e01
“Luciana <lsouzabianchini@gmail.com>
2022-03-30 15:09:58 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Hello Desafio Github API</h1>
+    <>
+      <Navbar />
+      <h2>Hello Desafio Github API</h2>
+    </>
   );
 }
 
---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+function Navbar() {
+  return (
+    <nav className="bg-primary main-nav">
+      <div>
+        <a href="link" className="nav-logo-text">
+          <h1>Github API</h1>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
